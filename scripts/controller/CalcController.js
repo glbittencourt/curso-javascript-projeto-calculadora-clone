a5e727b6bbe2ddf86c9fb6fcb3c89f0e7dfa5bf1
glbittencourt <glbittencourt26@gmail.com>
2025-01-28 16:32:27 -0300

    final projeto calculadora

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -2,6 +2,8 @@ class CalcController {
 
     constructor(){
 
+        this._audio = new Audio('click.mp3');
+        this._audioOnOff = false;
         this._lastOperator = '';
         this._lastNumber = '';
 
@@ -13,10 +15,41 @@ class CalcController {
         this._currentDate;
         this.initialize();
         this.initButtonsEvents();
+        this.initKetboard();
         
     }
 
-    initialize(){
+    pasteFromClipboard(){
+
+        document.addEventListener('paste', e=> {
+
+            let text = e.clipboardData.getData('Text');
+
+            this.displayCalc = parseFloat(text);
+
+            console.log(text);
+
+        })
+
+    }
+
+    copyToClipboard() {
+
+        let input = document.createElement('input');
+
+        input.value = this.displayCalc;
+
+        document.body.appendChild(input);
+
+        input.select();
+
+        document.execCommand("Copy");
+
+        input.remove();
+
+    }
+
+    initialize() {
 
         this.setDisplayDateTime();
 
@@ -27,7 +60,90 @@ class CalcController {
         }, 1000);
 
         this.setLastNumberToDisplay();
+        this.pasteFromClipboard();
     
+        document.querySelectorAll('.btn-ac').forEach(btn=>{
+
+            btn.addEventListener('dblclick', e=>{
+
+                this.toggleAudio();
+
+            });
+
+        })
+
+    }
+
+    toggleAudio(){
+
+        this._audioOnOff = !this._audioOnOff;
+
+    }
+
+    playAudio(){
+
+        if (this._audioOnOff) {
+
+            this._audio.currentTime = 0;
+            this._audio.play();
+
+        }
+
+    }
+
+    initKetboard() {
+
+        document.addEventListener('keyup', e=> {
+
+            this.playAudio();
+
+            console.log(e.key);
+
+            switch(e.key) {
+
+                case 'Escape':
+                    this.clearAll();
+                    break;
+                case 'Backspace':
+                    this.clearEntry();
+                    break;
+                case '+':
+                case '-':
+                case '/':
+                case '*':
+                case '%':
+                    this.addOperation(e.key);
+                    break;
+                case 'Enter':  
+                case '=':       
+                    this.calc();
+                    break;
+                case '.':
+                case ',':
+                    this.addDot();
+                    break;
+ 
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    this.addOperation(parseInt(e.key));
+                    break;
+
+                case 'c':
+                    if (e.ctrlKey) this.copyToClipboard();
+                    break;
+
+            }
+            
+        });
+        
     }
 
     addEventListenerAll(element, events, fn){
@@ -91,7 +207,16 @@ class CalcController {
 
     getResult() {
 
-        return eval(this._operation.join(""));
+        try {
+            return eval(this._operation.join(""));
+
+        } catch(e){
+            setTimeout(()=> {
+                this.setError();
+            }, 1);
+            
+        }
+
 
     }
 
@@ -237,6 +362,8 @@ class CalcController {
 
     execBtn(value){
 
+        this.playAudio();
+
         switch(value) {
 
             case 'ac':
@@ -284,8 +411,6 @@ class CalcController {
                 this.setError();
                 break;
 
-
-
         }
 
     }
@@ -296,8 +421,8 @@ class CalcController {
 
         buttons.forEach((btn, index)=> {
 
-            this.addEventListenerAll(btn, "click ", e =>{
-                //drag mouseover
+            this.addEventListenerAll(btn, "click drag", e =>{
+            
                 let textBtn = btn.className.baseVal.replace("btn-", "");
 
                 this.execBtn(textBtn);
@@ -354,6 +479,12 @@ class CalcController {
     }
 
     set displayCalc(value){
+
+        if (value.toString().length > 10) {
+            this.setError();
+            return false;
+
+        }
 
         this._displayCalcEl.innerHTML = value;
 
